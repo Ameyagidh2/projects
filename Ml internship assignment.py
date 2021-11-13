fc5f568dab79010463fa36f99d2621debcf27449
ameyagidh <65457905+ameyagidh@users.noreply.github.com>
2021-11-12 21:00:29 -0500

    Add files via upload

---
+++ Ml internship assignment.py
@@ -0,0 +1,6 @@
+#import os
+#os.chdir(r"")
+name=input("Enter your name ")
+weight=float(input("Enter your weight "))
+pounds = weight * 2.2
+print("{} has a weight of {} pounds".format(name,pounds))
